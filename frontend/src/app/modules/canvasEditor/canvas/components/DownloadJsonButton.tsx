d8355e6f0f8f805f1646c7d091e18f42ec001349
Aditya Peri <adityaperi@Adityas-MacBook-Pro-5.local>
2025-06-20 11:29:32 -0400

    DownloadJsonFunctioanlity

--- frontend/src/app/modules/canvasEditor/canvas/components/DownloadJsonButton.tsx
+++ frontend/src/app/modules/canvasEditor/canvas/components/DownloadJsonButton.tsx
@@ -8,9 +8,23 @@ interface Props {
 }
 
 const DownloadJsonButton: React.FC<Props> = ({ elements }) => {
+  const downloadJsonFile = () => {
+    const blob = new Blob([JSON.stringify(elements, null, 2)], {
+      type: 'application/json',
+    });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'canvas-data.json';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  };
 
   return (
-    <button className={styles.downloadJsonButton}>
+    <button onClick={downloadJsonFile} className={styles.downloadJsonButton}>
       Download JSON
     </button>
   );

6e8288b8959e2971c099aaef8c52c9e6de51b84d
Kevin Chen <109507575+kcccr123@users.noreply.github.com>
2025-07-17 09:37:59 -0400

    Update Canvas.tsx

--- frontend/src/modules/canvasEditor/canvas/Canvas.tsx
+++ frontend/src/modules/canvasEditor/canvas/Canvas.tsx
@@ -153,11 +153,10 @@ export default function Canvas({
     );
 
     setElements((prev) => {
-      const boxIds = prev.map(el => el.boxId as number).sort((a, b) => a - b);
-      let newBoxId = boxIds.length;         
-      for (let i = 0; i < boxIds.length; i++) {
-        if (boxIds[i] !== i) {               
-          newBoxId = i;
+      let newBoxId = prev.length;
+      for (let i = 0; i < prev.length - 1; i++) {
+        if ((prev[i].boxId as number) + 1 !== prev[i + 1].boxId) {
+          newBoxId = (prev[i].boxId as number) + 1;
           break;
         }
       }

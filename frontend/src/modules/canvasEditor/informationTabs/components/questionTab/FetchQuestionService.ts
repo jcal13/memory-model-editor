77eced155dd3faa49b21513c16124b7dd3704fde
Lily Meng <lilyxmeng13@gmai.com>
2025-07-31 19:49:44 -0400

    add question tab UI

--- frontend/src/modules/canvasEditor/informationTabs/components/questionTab/FetchQuestionService.ts
+++ frontend/src/modules/canvasEditor/informationTabs/components/questionTab/FetchQuestionService.ts
@@ -2,12 +2,20 @@ import axios from "axios";
 
 const API_DEV_URL = "http://localhost:3001";
 
-export async function fetchTestQuestionCount(): Promise<number> {
-  const res = await axios.post(`${API_DEV_URL}/questions/testquestions`);
-  return res.data.count;
+export async function fetchQuestion<T = any>(
+  id: number,
+  type: "test" | "practice"
+): Promise<T> {
+  const res = await axios.get(
+    `${API_DEV_URL}/questions/${type}questions/${id}`
+  );
+  return res.data as T;
 }
 
-export async function fetchTestQuestion(id: number) {
-  const res = await axios.get(`${API_DEV_URL}/questions/testquestions/${id}`);
-  return res.data;
+export async function fetchQuestionCount(
+  type: "test" | "practice"
+): Promise<number> {
+  const url = `${API_DEV_URL}/questions/${type}questions`;
+  const res = await axios.post(url);
+  return res.data.count as number;
 }

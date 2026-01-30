071d7561626e3a39332f30b20f9642214eeeb8d0
lilyxmeng <lilyxmeng13@gmail.com>
2026-01-30 14:22:59 -0500

    added production host

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,9 @@ import canvasEditorRouter from "./features/validationServices";
 import questionModuleRouter from "./features/informationTabs/questionTab";
 
 const app = express();
-const port = 3001;
+
+const port = process.env.PORT ? Number(process.env.PORT) : 3001;
+const host = process.env.HOST || "0.0.0.0";
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +17,6 @@ app.use(bodyParser.json());
 app.use("/canvasEditor", canvasEditorRouter);
 app.use("/questions", questionModuleRouter);
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`);
 });

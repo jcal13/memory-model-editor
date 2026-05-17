cd226496fab5f019942418c44f224d406e0017ad
Anbuselvan Ragunathan <anbuselvan.ragunathan@mail.utoronto.ca>
2026-05-17 11:34:22 -0400

    fixed an issue with another file

--- frontend/src/features/informationTabs/questionTab/components/CodeBlock.tsx
+++ frontend/src/features/informationTabs/questionTab/components/CodeBlock.tsx
@@ -31,7 +31,7 @@ export default function CodeBlock({
         >
           {tokens.map((line, index) => {
             const lineNumber = startLineNumber + index;
-            const { key: _lineKey, ...lineProps } = getLineProps({ line, key: index });
+            const lineProps = getLineProps({ line, key: index });
             const isCheckable = checkableLines?.has(lineNumber) ?? false;
             const isSelected = selectedLine === lineNumber;
 
@@ -72,10 +72,7 @@ export default function CodeBlock({
                   {line.map((token, tokenIndex) => (
                     <span
                       key={tokenIndex}
-                      {...(() => {
-                        const { key: _tokenKey, ...tokenProps } = getTokenProps({ token, key: tokenIndex });
-                        return tokenProps;
-                      })()}
+                      {...getTokenProps({ token, key: tokenIndex })}
                     />
                   ))}
                 </span>
